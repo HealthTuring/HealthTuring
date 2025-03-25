66acee33373930c0f9b70edd4bd5a58f8cb9b5f3
Alvalogue72 <alvalogue72@gmail.com>
2025-03-25 21:12:16 +0100

    Create server

---
+++ backend/healthturing-server/src/test/java/com/healthturing/healthturing_server/HealthturingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthturing.healthturing_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthturingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
