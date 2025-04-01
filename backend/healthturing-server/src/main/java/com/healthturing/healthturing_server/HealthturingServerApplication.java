c10494fd59581fcd717859e58065fc7dbf28a98c
JuanFranCM <jfchamac@gmail.com>
2025-04-01 22:24:58 +0200

    docker-compose and env start

--- backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/HealthturingServerApplication.java
+++ backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/HealthturingServerApplication.java
@@ -2,12 +2,17 @@ package com.healthturing.healthturing_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpaht:application.properties")
 public class HealthturingServerApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(HealthturingServerApplication.class, args);
+
+		
 	}
 
 }
