320ce92dcee4b73b4da9d413ed2f8787f0bfef58
JuanFranCM <jfchamac@gmail.com>
2025-04-06 13:57:09 +0200

    Swagger and back active

--- backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/HealthturingServerApplication.java
+++ backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/HealthturingServerApplication.java
@@ -2,10 +2,8 @@ package com.healthturing.healthturing_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpaht:application.properties")
 public class HealthturingServerApplication {
 
 	public static void main(String[] args) {
---
+++ backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/controllers/ApiController.java
@@ -0,0 +1,17 @@
+package com.healthturing.healthturing_server.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    @GetMapping("/prueba")
+    public String prueba() {
+        return "http://localhost:8080/swagger-ui/index.html#/api-controller/prueba";
+    }
+    
+}
