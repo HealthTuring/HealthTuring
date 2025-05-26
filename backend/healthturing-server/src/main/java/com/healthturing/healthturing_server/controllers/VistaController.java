6c00d10092d82bd4a52ff763856feca3577d304d
JuanFranCM <jfchamac@gmail.com>
2025-05-26 09:13:26 +0200

    Admin MVC v1

--- backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/controllers/VistaController.java
+++ backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/controllers/VistaController.java
@@ -1,10 +1,12 @@
 package com.healthturing.healthturing_server.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@PreAuthorize("hasAnyRole('ADMIN')")
 public class VistaController {
 
     @GetMapping("/vista")
@@ -18,6 +20,20 @@ public class VistaController {
         model.addAttribute("mensaje", "Redireccion a HOME");
         return "home"; // Este es el nombre del archivo HTML en /resources/templates
     }
+
+    @GetMapping("/landing")
+    public String mostrarLanding(Model model) {
+        model.addAttribute("mensaje", "Redireccion a HOME");
+        return "landing"; // Este es el nombre del archivo HTML en /resources/templates
+    }
+
+    @GetMapping("/prueba")
+    public String mostrarPrueba(Model model) {
+        model.addAttribute("mensaje", "Redireccion a HOME");
+        return "prueba"; // Este es el nombre del archivo HTML en /resources/templates
+    }
+
+
 
 
 }
