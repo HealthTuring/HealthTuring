150145427732ac0979ccdbafc0780c9c78b4a012
Alvalogue72 <alvalogue72@gmail.com>
2025-06-06 09:54:13 +0200

    refactor code

--- backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/configs/WebSocketConfiguration.java
+++ backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/configs/WebSocketConfiguration.java
@@ -1,5 +1,6 @@
 package com.healthturing.healthturing_server.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,6 +11,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${url.client}")
+    private String allowedOrigins;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
@@ -18,7 +22,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat-socket").setAllowedOrigins("http://localhost:4200").withSockJS();
+        registry.addEndpoint("/chat-socket").setAllowedOrigins(allowedOrigins).withSockJS();
     }
-    
+
 }
