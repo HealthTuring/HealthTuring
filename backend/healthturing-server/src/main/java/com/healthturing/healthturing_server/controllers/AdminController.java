99897557e9193de94d94d2e73ae2fb89f13e5623
JuanFranCM <jfchamac@gmail.com>
2025-06-13 00:57:54 +0200

    back fix v1

--- backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/controllers/AdminController.java
+++ backend/healthturing-server/src/main/java/com/healthturing/healthturing_server/controllers/AdminController.java
@@ -116,7 +116,7 @@ public class AdminController {
         return "redirect:/admin/patient-requests";
     }
 
-    @GetMapping()
+    @GetMapping("/")
     public String homeLanding(Model model) {
         model.addAttribute("mensaje", "Redireccion a landing");
         return "landing"; 
